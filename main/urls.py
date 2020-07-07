4f54a7731239f4d171474e050bd40763e790aeb7
Tushar <tushar.mohan2001@gmail.com>
2020-07-07 19:34:02 +0530

    class based views

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     
     path('',views.index),
+    path('class',views.UserListView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.views.generic import ListView
 def index(request):
     user_list=User.objects.all()
     page=request.GET.get('page',1)
@@ -15,3 +15,10 @@ def index(request):
     except EmptyPage:
         users=paginator.page(paginator.nunm_pages)
     return render(request,'index.html',{'users':users})
+
+class UserListView(ListView):
+    model=User
+    template_name='indexClass.html'
+    context_object_name='users' #default: object_list
+    paginate_by=1
+    queryset=User.objects.all() #default : Models.objects.all()
